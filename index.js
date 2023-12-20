106f545a8fd95a7eb2147bdd4defaeda66037b3a
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2023-12-19 21:48:31 -0500

    added code to index.js: init funtion and generateLogo function

---
+++ index.js
@@ -0,0 +1,24 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const questions = require('./lib/questions.js');
+
+// generateLogo function creates svg logo using inquirer response and fs
+function generateLogo(response){
+    const svg = setShape(response);
+    fs.writeFile("logo.svg", svg, () => console.log("Successfully generated logo!"))
+}
+
+// initializes generateLogo function using responses
+function init () {
+inquirer
+.prompt(questions)
+.then ((response) => {
+    generateLogo(response);
+})
+// catches errors if any
+.catch (err => {
+    console.log(err)
+});
+}
+
+init();
